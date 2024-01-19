ac985f0fcfda0ee1c4236a6df62cec16640d01be
Chris Nelson <chrisnelson7791@gmail.com>
2024-01-18 23:19:53 -0800

    First commit. Working prototype that console logs converted notes from key G to key C.

---
+++ main.js
@@ -0,0 +1,48 @@
+class Kalimba {
+  constructor(brand, model, key, tinesNum, notesArr) {
+    this.brand = brand
+    this.model = model
+    this.key = key
+    this.tinesNum = tinesNum
+    this.notesArr = notesArr
+  }
+
+  keyConverter(strOfNotes, desiredKey) {
+    // declare variable convertedNotes set it to an empty array
+    // split strOfNotes into array by space
+    // loop through the strOfNotes array 
+      // find the index of the current note in the current key array // indexOf()
+      // push the desired key array[j] to convertedNotes
+    // join convertedNotes with a space between each element
+    // return convertedNotes
+
+    let convertedNotes = []
+
+    strOfNotes = strOfNotes.split(' ')
+    // console.log(strOfNotes)
+
+    for (let i = 0; i < strOfNotes.length; i++) {
+      // console.log(this.notes[0])
+      // console.log(desiredKey[ this.notesArr.indexOf( strOfNotes[i] ) ])
+      // convertedNotes.push( desiredKey[ desiredKey.indexOf( strOfNotes[i] ) ] )
+
+      convertedNotes.push( desiredKey[ this.notesArr.indexOf( strOfNotes[i] ) ] )
+    }
+
+    // console.log(convertedNotes)
+    return convertedNotes.join(' ')
+
+
+
+  }
+
+}
+
+const G = ['G0', 'A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2']
+const C = ['C0', 'D0', 'E0', 'F0', 'G0', 'A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'A2', 'B2', 'C2', 'D2', 'E2']
+
+let myKalimba = new Kalimba('Hugh Tracey', 'iForget', 'G', 15, G)
+let misaKalimba = new Kalimba('n/a', 'n/a', 'C', 17, C)
+
+
+// function keyConverter()
